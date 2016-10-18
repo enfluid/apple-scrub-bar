79dc7a6c76cc440e1735c9ca86bbb06f1e817fa8
Russ Kamalov <ruslankmlv@gmail.com>
2016-10-17 18:58:49 -0700

    Add SlidingSegmentedControl

---
+++ SlidingSegmentedControl/SlidingSegmentedControl.swift
@@ -0,0 +1,9 @@
+//
+//  SlidingSegmentedControl.swift
+//  SlidingSegmentedControl
+//
+//  Created by Ruslan Kamalov on 10/17/16.
+//  Copyright © 2016 Ruslan Kamalov. All rights reserved.
+//
+
+import Foundation
